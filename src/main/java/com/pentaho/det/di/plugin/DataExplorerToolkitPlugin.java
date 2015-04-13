1c508a86df40f8944dddbaec26f07cc3e9a4b876
Guilherme Raimundo <guilherme.raimundo@webdetails.pt>
2015-04-13 18:31:45 +0100

    [SERVDEV-271] - Created initial project structure

---
+++ src/main/java/com/pentaho/det/di/plugin/DataExplorerToolkitPlugin.java
@@ -0,0 +1,48 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2015 Pentaho Corporation. All rights reserved.
+ */
+
+package com.pentaho.det.di.plugin;
+
+import org.pentaho.di.ui.spoon.SpoonPlugin;
+import org.pentaho.di.ui.spoon.SpoonPluginCategories;
+
+import org.pentaho.di.ui.spoon.SpoonLifecycleListener;
+import org.pentaho.di.ui.spoon.SpoonPerspective;
+import org.pentaho.di.ui.spoon.SpoonPluginInterface;
+import org.pentaho.ui.xul.XulDomContainer;
+import org.pentaho.ui.xul.XulException;
+
+@SpoonPlugin( id = "pentaho-det",
+    description = "Pentaho Data Explorer Toolkit",
+    name = "Pentaho Data Explorer Toolkit",
+    image = "" )
+@SpoonPluginCategories( { "spoon" } )
+public class DataExplorerToolkitPlugin implements SpoonPluginInterface {
+
+  public void applyToContainer( String s, XulDomContainer xulDomContainer ) throws XulException {
+
+  }
+
+  public SpoonLifecycleListener getLifecycleListener() {
+    return null;
+  }
+
+  public SpoonPerspective getPerspective() {
+    return null;
+  }
+
+}
