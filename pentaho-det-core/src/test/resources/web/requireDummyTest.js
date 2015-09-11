b579e6687bc98b6cd156aa4bf87ecd2ffb441ad7
Nelson Antunes <nelson.antunes@webdetails.pt>
2015-09-11 15:23:45 +0100

    [SERVDEV-356] Jasmine Hello World test

--- pentaho-det-core/src/test/resources/web/requireDummyTest.js
+++
@@ -1,7 +0,0 @@
-define(function() {
-    describe("Require Hello World Resource Folder", function() {
-        it("Hello is not World", function() {
-            expect("Hello" != "World").toBe(true);
-        })
-    });
-});
